b96517b229f9e9f9cdb8f9fdd6bee1c7969db209
Subham2804 <39151404+Subham2804@users.noreply.github.com>
2020-01-30 21:25:25 +0530

    Update tinderautoliker.py
    update auto liking and disliking duration

--- tinderautoliker.py
+++ tinderautoliker.py
@@ -5,8 +5,7 @@ import random
 from random import randint
 
 """
-Free Version: 100 swipes per 12-hour period.
-Paid Version: Unlimited swipes
+Free Version: 1000 swipes per 12-hour period.
 How to: User must be logged in prior start
         Works best when in full screen mode in browser"""
 
@@ -19,8 +18,8 @@ chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 # chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 # Linux
-# chrome_path = '/usr/bin/google-chrome %s'
-tim= randint(5,15)
+# chrome_path = '/usr/bin/google-chrome %s' 
+tim= randint(5,10)
 webbrowser.get(chrome_path).open(url)
 time.sleep(tim)
 pyautogui.click(button='right')
@@ -31,8 +30,10 @@ for x in range(0, 1000):
 	y = randint(0, 1)
 	if y == 0:
 		pyautogui.press('left')
+		time.sleep(tim)
 	elif y == 1:
 		pyautogui.press('right')
+		time.sleep(tim)
 	elif x == 100:
 		print ("EXIT")
 		break
